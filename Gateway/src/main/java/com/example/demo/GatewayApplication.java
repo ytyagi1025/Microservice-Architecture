d2efa7ab7886751c008cea2b7e698febd3fa01a0
ytyagi1025 <ytyagi1025@gmail.com>
2021-06-27 13:07:27 +0530

    Gateway Commit

---
+++ Gateway/src/main/java/com/example/demo/GatewayApplication.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator getRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes().route(t -> t.path("/getEmp/**").uri("lb://EMPLOYEE"))
+				.route(t -> t.path("/getTech/**").uri("lb://TECHNOLOGY")).build();
+	}
+
+}
